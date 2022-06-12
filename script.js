8ae40f42d9beee87380fd61ea545400631df1340
RodrigoCosta <costardgg@gmail.com>
2022-06-11 21:02:02 -0300

    atualizacao4

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-function selecao (){ 
-    
+function activeBorder(element){
 
-if ( icon.style.display === "none" );{
-    icon.style.display === "block"
+    let selector = document.querySelector('.conteinerfeed .selecionar .icon2')
+    if(selector !== null){
+        selector.classList.remove('.selecionar')
+    }
+    element.classList.add('selecionar')
+
+    if(selector !== null){
+        selector.classList.remove('.icon2')
+    }
+    element.classList.add('icon2')
+
 }
 
+function activeBorder2(element){
+
+    let selector = document.querySelector('.conteinerfeed2 .selecionar');
+    if(selector !== null){
+        selector.classList.remove('.selecionar');
+    }
+
+    else {element.classList.add('.selecionar')
+    activeGreen ()
+    }
+}
+
+function activeBorder3(element){
+    let selector = document.querySelector('.conteinerfeed3 .selecionar')
+    if(selector !== null){
+        selector.classList.remove('.selecionar')
+    }
+    element.classList.add('.selecionar')
+    activeGreen ()
 }
