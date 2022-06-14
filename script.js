501680e9671aac2f98d64f63758241fa25cba9b8
RodrigoCosta <costardgg@gmail.com>
2022-06-13 21:10:46 -0300

    atualizacao5

--- script.js
+++ script.js
@@ -1,35 +1,9 @@
-function activeBorder(element){
-
-    let selector = document.querySelector('.conteinerfeed .selecionar .icon2')
-    if(selector !== null){
-        selector.classList.remove('.selecionar')
-    }
-    element.classList.add('selecionar')
-
-    if(selector !== null){
-        selector.classList.remove('.icon2')
-    }
-    element.classList.add('icon2')
-
-}
-
-function activeBorder2(element){
-
-    let selector = document.querySelector('.conteinerfeed2 .selecionar');
-    if(selector !== null){
-        selector.classList.remove('.selecionar');
-    }
-
-    else {element.classList.add('.selecionar')
-    activeGreen ()
-    }
-}
-
-function activeBorder3(element){
-    let selector = document.querySelector('.conteinerfeed3 .selecionar')
-    if(selector !== null){
-        selector.classList.remove('.selecionar')
-    }
-    element.classList.add('.selecionar')
-    activeGreen ()
-}
+ function selecionarprato(nomeClasse) {
+     const botaoclicado = document.querySelector (".selecionado")
+     if (botaoclicado !== null) {
+        botaoclicado.classList.remove("selecionado")
+     }
+     
+        const botao = document.querySelector(`.${nomeClasse}`);
+        botao.classList.add("selecionado");
+    } 
